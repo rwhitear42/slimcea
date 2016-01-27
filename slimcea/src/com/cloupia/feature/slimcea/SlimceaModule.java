0a32431ac42b79e7a74125736439b502479a35b2
Russ Whitear <russ@whitear.co.uk>
2016-01-27 18:06:43 +0000

    Rollback working.

--- slimcea/src/com/cloupia/feature/slimcea/SlimceaModule.java
+++ slimcea/src/com/cloupia/feature/slimcea/SlimceaModule.java
@@ -26,6 +26,7 @@ import com.cloupia.feature.slimcea.tasks.HelloWorldTask;
 import com.cloupia.feature.slimcea.tasks.MultiSelectTabularTask;
 import com.cloupia.feature.slimcea.tasks.RollbackHelloWorldTask;
 import com.cloupia.feature.slimcea.tasks.SlimceaCreateVolumeTask;
+import com.cloupia.feature.slimcea.tasks.SlimceaDeleteVolumeTask;
 import com.cloupia.feature.slimcea.tasks.SlimceaTaskContextTask;
 import com.cloupia.feature.slimcea.triggers.MonitorDummyDeviceStatusParam;
 import com.cloupia.feature.slimcea.triggers.MonitorDummyDeviceType;
@@ -60,7 +61,8 @@ public class SlimceaModule extends AbstractCloupiaModule {
 		AbstractTask task7 = new SlimceaTaskContextTask();
 		AbstractTask task8 = new MultiSelectTabularTask();
 		AbstractTask task9 = new SlimceaCreateVolumeTask();
-		AbstractTask[] tasks = new AbstractTask[9];
+		AbstractTask task10 = new SlimceaDeleteVolumeTask();
+		AbstractTask[] tasks = new AbstractTask[10];
 		tasks[0] = task1;
 		tasks[1] = task2;
 		tasks[2] = task3;
@@ -70,6 +72,7 @@ public class SlimceaModule extends AbstractCloupiaModule {
 		tasks[6] = task7;
 		tasks[7] = task8;
 		tasks[8] = task9;
+		tasks[9] = task10;
 		return tasks;
 	}
 
--- slimcea/src/com/cloupia/feature/slimcea/tasks/SlimceaDeleteVolumeTask.java
+++ slimcea/src/com/cloupia/feature/slimcea/tasks/SlimceaDeleteVolumeTask.java
@@ -7,42 +7,38 @@ import com.cloupia.service.cIM.inframgr.customactions.CustomActionLogger;
 import com.cloupia.service.cIM.inframgr.customactions.CustomActionTriggerContext;
 
 
-public class SlimceaCreateVolumeTask extends AbstractTask {
+public class SlimceaDeleteVolumeTask extends AbstractTask {
 
 	@Override
 	public void executeCustomAction(CustomActionTriggerContext context,
 			CustomActionLogger actionLogger) throws Exception {
-		SlimceaCreateVolumeConfig config = (SlimceaCreateVolumeConfig) context.loadConfigObject();
+		SlimceaDeleteVolumeConfig config = (SlimceaDeleteVolumeConfig) context.loadConfigObject();
 
 		actionLogger.addInfo("Username: " +config.getUsername());
 		actionLogger.addInfo("Password: " +config.getPassword());
 		actionLogger.addInfo("IP Address: " +config.getIpAddress());
 		actionLogger.addInfo("Volume Name: " +config.getVolumeName());
-		actionLogger.addInfo("Volume Size (GB): " +config.getVolumeSizeGB());
-		actionLogger.addInfo("Volume Description: " +config.getVolumeDescription());
-		actionLogger.addInfo("Data Encryption: " +config.getVolumeDataEncryption());
-		actionLogger.addInfo("Cache Pinning: " +config.getVolumeCachePinning());
-		actionLogger.addInfo("Performance Policy: " +config.getVolumePerfPolicy());
 		
 		//Session session = jsch.getSession(config.getUsername(), config.getIpAddress(), 22);
 		//session.setPassword(config.getPassword());
-			
+
+				
 		//if user decides to rollback a workflow containing this task, then using the change tracker
 		//we can take care of rolling back this task (i.e, disabling snmp)
 		//NOTE: use the getTaskType() method in your handler to pass as the 5th argument
-		context.getChangeTracker().undoableResourceAdded("assetType", "idString", "Nimble Create Volume", 
-				"Rollback Create Nimble Volume " + config.getVolumeName(), 
-				new SlimceaDeleteVolumeTask().getTaskName(), new SlimceaDeleteVolumeConfig(config));
+		//context.getChangeTracker().undoableResourceAdded("assetType", "idString", "SNMP enabled", 
+		//		"SNMP enabled on " + config.getIpAddress(), 
+		//		new DisableSNMPNexusTask().getTaskName(), new DisableSNMPNexusConfig(config));
 	}
 
 	@Override
 	public TaskConfigIf getTaskConfigImplementation() {
-		return new SlimceaCreateVolumeConfig();
+		return new SlimceaDeleteVolumeConfig();
 	}
 
 	@Override
 	public String getTaskName() {
-		return SlimceaCreateVolumeConfig.displayLabel;
+		return SlimceaDeleteVolumeConfig.displayLabel;
 	}
 
 	@Override
