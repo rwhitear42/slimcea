428cb5a27283ca0f507d2babb9faf0fbcab58f09
Russ Whitear <russ@whitear.co.uk>
2016-02-11 07:49:48 +0000

    Map volume to IGroup task working.

--- slimcea/src/com/cloupia/feature/slimcea/tasks/SlimceaMapVolumeToIgroupTask.java
+++ slimcea/src/com/cloupia/feature/slimcea/tasks/SlimceaMapVolumeToIgroupTask.java
@@ -1,11 +1,16 @@
 package com.cloupia.feature.slimcea.tasks;
 
-import com.cloupia.feature.slimcea.constants.SlimceaConstants;
 import com.cloupia.service.cIM.inframgr.AbstractTask;
 import com.cloupia.service.cIM.inframgr.TaskConfigIf;
 import com.cloupia.service.cIM.inframgr.TaskOutputDefinition;
 import com.cloupia.service.cIM.inframgr.customactions.CustomActionLogger;
 import com.cloupia.service.cIM.inframgr.customactions.CustomActionTriggerContext;
+import com.rwhitear.nimbleRest.accessControlRecords.CreateAccessControlRecord;
+import com.rwhitear.nimbleRest.authenticate.GetSessionToken;
+import com.rwhitear.nimbleRest.initiatorGroups.GetInitiatorGroups;
+import com.rwhitear.nimbleRest.initiatorGroups.json.GetInitiatorGroupsDetailResponse;
+import com.rwhitear.nimbleRest.volumes.GetVolumes;
+import com.rwhitear.nimbleRest.volumes.json.GetVolumesSummaryResponse;
 
 
 public class SlimceaMapVolumeToIgroupTask extends AbstractTask {
@@ -15,12 +20,48 @@ public class SlimceaMapVolumeToIgroupTask extends AbstractTask {
 			CustomActionLogger actionLogger) throws Exception {
 		SlimceaMapVolumeToIgroupConfig config = (SlimceaMapVolumeToIgroupConfig) context.loadConfigObject();
 
-		actionLogger.addInfo("Username: " +config.getUsername());
-		actionLogger.addInfo("Password: " +config.getPassword());
-		actionLogger.addInfo("IP Address: " +config.getIpAddress());
-		actionLogger.addInfo("Volume Name: " +config.getVolumeName());
-		actionLogger.addInfo("Initiator Group Name: " +config.getInitiatorGroupName());
+		
+		String ipAddress = config.getIpAddress();		
+		String username = config.getUsername();
+		String password = config.getPassword();
+		String volumeName = config.getVolumeName();
+		String initiatorGroupName = config.getInitiatorGroupName();
+		
+		
+		// Retrieve Nimble array auth token.
+		String token = new GetSessionToken(ipAddress, username, password).getNewToken();
+		
+		
+		// Get volume ID for 'volumeName'.
+		actionLogger.addInfo("Retrieving volume ID for volume ["+ volumeName + "].");
+		
+		String volumeJsonData = new GetVolumes(ipAddress, token).getSummary();
+				
+		String volID = new GetVolumesSummaryResponse(volumeJsonData).getVolumeID(volumeName);
 
+		
+		// Initiator Groups
+		actionLogger.addInfo("Retrieving Initiator Group ID for iGroup ["+ initiatorGroupName + "].");
+		
+		String iGroupJsonData = new GetInitiatorGroups(ipAddress, token).getInitiatorGroupSummary();
+		
+		//actionLogger.addInfo("iGroupJsonData: " + iGroupJsonData);
+		
+		String iGroupID = new GetInitiatorGroupsDetailResponse(iGroupJsonData).getInitiatorGroupID(initiatorGroupName);
+
+		//actionLogger.addInfo("iGroupID: " + iGroupID );
+		
+		
+		// Create new ACR.
+		if( !volID.equals(null) ) {
+			
+			actionLogger.addInfo("Creating new Access Control Record for volume ["+ volumeName + "] and Initiator Group [" + initiatorGroupName + "].");
+			
+			actionLogger.addInfo("Create ACR JSON Response:\n"  + new CreateAccessControlRecord(ipAddress, token, volID, iGroupID).create());
+		}
+
+		
+		/*
 		//if user decides to rollback a workflow containing this task, then using the change tracker
 		//we can take care of rolling back this task (i.e, disabling snmp)
 		//NOTE: use the getTaskType() method in your handler to pass as the 5th argument
@@ -36,6 +77,7 @@ public class SlimceaMapVolumeToIgroupTask extends AbstractTask {
         {
             actionLogger.addWarning("Failed to set output variable(s): " + e.getMessage());
         }
+        */
 	}
 
 	@Override
@@ -50,6 +92,12 @@ public class SlimceaMapVolumeToIgroupTask extends AbstractTask {
 
 	@Override
 	public TaskOutputDefinition[] getTaskOutputDefinitions() {
+		return null;
+	}
+	
+	/*
+	@Override
+	public TaskOutputDefinition[] getTaskOutputDefinitions() {
 		TaskOutputDefinition[] ops = new TaskOutputDefinition[1];
 		//NOTE: If you want to use the output of this task as input to another task. Then the second argument 
 		//of the output definition MUST MATCH the type of UserInputField in the config of the task that will
@@ -57,4 +105,5 @@ public class SlimceaMapVolumeToIgroupTask extends AbstractTask {
 		ops[0] = new TaskOutputDefinition( SlimceaConstants.NIMBLE_INITIATOR_GROUP_NAME, SlimceaConstants.GENERIC_TEXT_INPUT, "Three" );
 		return ops;
 	}
+	*/
 }
