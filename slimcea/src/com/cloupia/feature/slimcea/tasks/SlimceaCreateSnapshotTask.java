e5def1e6021fa764071e76718e84da6d22dde049
Russ Whitear <russ@whitear.co.uk>
2016-02-17 10:58:49 +0000

    v0.9.9 - Snapshots with rollback

--- slimcea/src/com/cloupia/feature/slimcea/tasks/SlimceaCreateSnapshotTask.java
+++ slimcea/src/com/cloupia/feature/slimcea/tasks/SlimceaCreateSnapshotTask.java
@@ -5,6 +5,15 @@ import com.cloupia.service.cIM.inframgr.TaskConfigIf;
 import com.cloupia.service.cIM.inframgr.TaskOutputDefinition;
 import com.cloupia.service.cIM.inframgr.customactions.CustomActionLogger;
 import com.cloupia.service.cIM.inframgr.customactions.CustomActionTriggerContext;
+import com.rwhitear.nimbleRest.authenticate.GetSessionToken;
+import com.rwhitear.nimbleRest.exceptions.SnapshotIdException;
+import com.rwhitear.nimbleRest.exceptions.VolumeIdException;
+import com.rwhitear.nimbleRest.snapshots.CreateSnapshot;
+import com.rwhitear.nimbleRest.snapshots.GetSnapshots;
+import com.rwhitear.nimbleRest.snapshots.json.GetSnapshotDetailResponse;
+import com.rwhitear.nimbleRest.volumes.GetVolumes;
+import com.rwhitear.nimbleRest.volumes.json.ParseVolumeDetailResponse;
+import com.rwhitear.nimbleRest.volumes.json.VolumesDetailJsonObject;
 
 
 public class SlimceaCreateSnapshotTask extends AbstractTask {
@@ -14,6 +23,7 @@ public class SlimceaCreateSnapshotTask extends AbstractTask {
 			CustomActionLogger actionLogger) throws Exception {
 		SlimceaCreateSnapshotConfig config = (SlimceaCreateSnapshotConfig) context.loadConfigObject();
 
+		/*
 		actionLogger.addInfo("Username: " +config.getUsername());
 		actionLogger.addInfo("Password: " +config.getPassword());
 		actionLogger.addInfo("IP Address: " +config.getIpAddress());
@@ -22,9 +32,82 @@ public class SlimceaCreateSnapshotTask extends AbstractTask {
 		actionLogger.addInfo("Snapshot Description: " +config.getSnapshotDescription());
 		actionLogger.addInfo("Online: " +config.getSnapshotOnline());
 		actionLogger.addInfo("Writable: " +config.getSnapshotWritable());
+		*/
 		
-		//Session session = jsch.getSession(config.getUsername(), config.getIpAddress(), 22);
-		//session.setPassword(config.getPassword());
+		
+		String  ipAddress 		= config.getIpAddress();
+		String  username 		= config.getUsername();
+		String  password 		= config.getPassword(); 
+		String  volumeName 		= config.getVolumeName(); 	
+		String  snapShotName	= config.getSnapshotName();
+		String  description 	= config.getSnapshotDescription();
+		boolean online			= config.getSnapshotOnline();
+		boolean writable		= config.getSnapshotWritable();
+		
+		
+		// Get token.
+		// Retrieve Nimble array auth token.
+		String token = new GetSessionToken(ipAddress, username, password).getNewToken();
+
+		
+		// Get volumeID for volumeName.
+		String gvResponse = new GetVolumes(ipAddress, token).getDetail();
+		
+		actionLogger.addInfo("GetVolumes JSON: " + gvResponse );		
+		
+		// Parse JSON for volumeID for volumeName.
+		VolumesDetailJsonObject pvsr = new ParseVolumeDetailResponse(gvResponse).parse();
+		
+		String volumeID = "";
+		
+		for( int i=0; i<pvsr.getData().size(); i++ ) {
+			
+			if( pvsr.getData().get(i).getName().equals(volumeName)) {
+				
+				volumeID = pvsr.getData().get(i).getId();
+				
+				break;
+				
+			}
+			
+		}
+		
+		if( volumeID == "" ) {
+			
+			throw new VolumeIdException("Failed to find volume ID for volume [" +volumeName+ "].");
+				
+		} else {
+			
+			actionLogger.addInfo("VolID for [" +volumeName+ "] is [" +volumeID+ "].");
+			
+		}
+
+
+		// Check that the snapshot doesn't already exist.
+		// Now that we have the volumeID, we can go ahead and get the snapshot ID for snapShotName.
+		String gsResponse = new GetSnapshots(ipAddress, token, volumeID).getDetail();
+
+		actionLogger.addInfo("GetSnapshots JSON: " +gsResponse );
+		
+		GetSnapshotDetailResponse gsdr = new GetSnapshotDetailResponse(gsResponse);
+		
+		String snapID = gsdr.getSnapshotID(snapShotName);
+		
+		if( snapID != null ) {
+			
+			throw new SnapshotIdException("The snapshot [" +snapShotName+ "] already exists." );
+
+		}
+
+		// Snapshot doesn't exist, go ahead and create it.
+		
+		CreateSnapshot cs = new CreateSnapshot(ipAddress, token, snapShotName, volumeID, description, online, writable);
+		
+		String csResponse = cs.execute();
+		
+		actionLogger.addInfo("Create Snapshot Response: " + csResponse );
+
+		
 			
 		//if user decides to rollback a workflow containing this task, then using the change tracker
 		//we can take care of rolling back this task (i.e, disabling snmp)
